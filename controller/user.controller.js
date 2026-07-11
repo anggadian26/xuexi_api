2250e398d4162c66c71ed951a8f106b3b7c02c12
Angga Dian <anggadiansap@gmail.com>
2026-07-11 11:17:15 +0700

    read, readByID, Create -> users

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,14 +1,60 @@
+const { User } = require('../models')
+
+
 // -- CREATE USER (Sign Up)
 function signup(req, res, next){
-   res.render('index', {title: 'Sign Up User'})
+   const data = {
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      fullname: req.body.fullname,
+      picture: req.body.picture,
+      bio: req.body.bio,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      createdBy: 0,
+      updatedBy: 0,
+      isDeleted: false
+   }
+
+   User.create(data).then(result => {
+      res.status(201).json({
+         message: 'Success',
+         data: result
+      })
+   }).catch(err => {
+      res.status(500).json({
+         message: 'Register Failed',
+         data: err
+      })
+   })
 }
 // -- READ USER 
 function read(req, res, next) {
-   res.render('index', {title: 'Read User Data'})
+   User.findAll().then(users => {
+      res.send(users)
+   }).catch(err => {
+      res.send(err)
+   });
 }
 // -- READ USER BY ID
 function readById(req, res, next) {
-   res.render('index', {title: `Read User Data By ID ${req.params.id}`})
+   // --- Dipakai jika tidak menggunakan PK 
+   // User.findAll({
+   //    where: {id: req.params.id}
+   // }).then(users => {
+   //    res.send(users)
+   // }).catch(err => {
+   //    res.send(err)
+   // });
+
+
+   const id = req.params.id
+   User.findByPk(id).then(users => {
+      res.send(users)
+   }).catch(err => {
+      res.send(err)
+   })
 }
 // -- UPDATE USER 
 function update(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const userController  = require('../controller/user.controller')
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.read);
+router.post('/', userController.signup);
+router.get('/signin', userController.signin);
+router.get('/update/:id', userController.update);
+router.get('/delete/:id', userController.destroy);
+router.get('/:id', userController.readById);
 
 module.exports = router;
