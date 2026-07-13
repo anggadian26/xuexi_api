9023f2b5e4647e0ee91737c363e801e8d0ef0103
Angga Dian <anggadiansap@gmail.com>
2026-07-13 22:21:24 +0700

    enkripsi data password

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,69 +1,75 @@
 const { User } = require('../models')
 const Validator = require("fastest-validator");
-
+const bcrypt = require('bcrypt');
 const v = new Validator();
 
 
 // -- CREATE USER (Sign Up)
 function signup(req, res, next){
-   const data = {
-      username: req.body.username,
-      password: req.body.password,
-      email: req.body.email,
-      fullname: req.body.fullname,
-      picture: req.body.picture,
-      bio: req.body.bio,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      createdBy: 0,
-      updatedBy: 0,
-      isDeleted: false
-   }
 
-   const schema = {
-      username: { type: "string", min: 5, max: 50, optional: false},
-      email: { type: "email", optional: false},
-      password: { type: "string", min: 5, max: 225, optional: false},
-   }
-
-   // -- Cek Email
-   User.findOne({ where: {email: req.body.email} }).then(user => {
-      if(user) {
-         //  email sudah digunakan
-         res.status(400).json({
-            message: 'Email already exist',
-         })
-      } else {
-         // penggunaan validasi 
-         const validationResult = v.validate(data, schema);
-
-         if (validationResult !== true) {
-            res.status(400).json({
-               message: 'Validation Failed',
-               data: validationResult
-            });
-         } else {
-            // create user
-            User.create(data).then(result => {
-               res.status(201).json({
-                  message: 'Success',
-                  data: result
-               })
-            }).catch(err => {
-               res.status(500).json({
-                  message: 'Register Failed',
-                  data: err
-               })
-            })
+   // enkripsi data
+   bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(req.body.password, salt, function(err, hash) {
+         
+         const data = {
+            username: req.body.username,
+            password: hash,
+            email: req.body.email,
+            fullname: req.body.fullname,
+            picture: req.body.picture,
+            bio: req.body.bio,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            createdBy: 0,
+            updatedBy: 0,
+            isDeleted: false
          }
-      }
-   }).catch(err => {
-      res.status(500).json({
-         message: 'Something wrong',
-         data: err
+      
+         const schema = {
+            username: { type: "string", min: 5, max: 50, optional: false},
+            email: { type: "email", optional: false},
+            password: { type: "string", min: 5, max: 225, optional: false},
+         }
+      
+         // -- Cek Email
+         User.findOne({ where: {email: req.body.email} }).then(user => {
+            if(user) {
+               //  email sudah digunakan
+               res.status(400).json({
+                  message: 'Email already exist',
+               })
+            } else {
+               // penggunaan validasi 
+               const validationResult = v.validate(data, schema);
+      
+               if (validationResult !== true) {
+                  res.status(400).json({
+                     message: 'Validation Failed',
+                     data: validationResult
+                  });
+               } else {
+                  // create user
+                  User.create(data).then(result => {
+                     res.status(201).json({
+                        message: 'Success',
+                        data: result
+                     })
+                  }).catch(err => {
+                     res.status(500).json({
+                        message: 'Register Failed',
+                        data: err
+                     })
+                  })
+               }
+            }
+         }).catch(err => {
+            res.status(500).json({
+               message: 'Something wrong',
+               data: err
+            })
+         });
       })
-   });
-
+   })
 }
 // -- READ USER 
 function read(req, res, next) {
@@ -175,17 +181,21 @@ function signin(req, res, next) {
    User.findOne({where: {email: req.body.email}}).then(user => {
       if(user) {
          if(user.isDeleted == false){
-            if(user.password == req.body.password) {
-               res.status(200).json({
-                  message: 'Success',
-                  data: user
-               })
-            } else {
-               res.status(401).json({
-                  message: 'Wrong Password',
-                  data: user
-               })
-            }
+            bcrypt.compare(req.body.password, user.password, function(err, result) {
+               if(result) {
+                  res.status(200).json({
+                     status: "SUCCESS",
+                     message: 'Success Login',
+                     data: user
+                  })
+               } else {
+                  res.status(401).json({
+                     status: "FAILED",
+                     message: "Wrong Password",
+                     data: err
+                  })
+               }
+            })
          } else {
             res.status(401).json({
                message: 'User has been deleted',
